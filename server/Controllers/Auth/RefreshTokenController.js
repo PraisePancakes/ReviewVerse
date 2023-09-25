9041017e2df815418f7d73396a08643211b5c2b0
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2023-09-25 19:39:43 -0400

    refresh token route implemented

---
+++ server/Controllers/Auth/RefreshTokenController.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY;
+const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+const REFRESH_TOKEN_SECRET_KEY = process.env.REFRESH_TOKEN_SECRET_KEY;
+
+module.exports = async (req, res) => {
+  const _RT = req.cookies._RT;
+  const userId = req.id;
+  try {
+    jwt.verify(_RT, REFRESH_TOKEN_SECRET_KEY, (err) => {
+      if (err) {
+        return res
+          .status(406)
+          .send({ message: "_RT has an invalid signature or is expired" });
+      }
+
+      res.clearCookie("_AT", {
+        expires: new Date(0),
+        path: "/",
+        httpOnly: true,
+        sameSite: "lax",
+      });
+
+      const ACCESS_TOKEN = jwt.sign(
+        {
+          id: userId,
+        },
+        ACCESS_TOKEN_SECRET_KEY,
+        {
+          expiresIn: ACCESS_TOKEN_EXPIRY,
+        }
+      );
+
+      res.cookie("_AT", ACCESS_TOKEN, {
+        path: "/",
+        httpOnly: true,
+        expiresIn: new Date(Date.now() + 60000 * 60 * 24 * 24 * 6),
+        sameSite: "lax",
+      });
+
+      return res.send({
+        message: "_AT has been refreshed",
+      });
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Internal Server Error! " });
+  }
+};
--- server/Routes/Auth/AuthRoutes.js
+++ server/Routes/Auth/AuthRoutes.js
@@ -4,9 +4,11 @@ const registerController = require("../../Controllers/Auth/RegisterController");
 const loginController = require("../../Controllers/Auth/LoginController");
 const verifyToken = require("../../Middlewares/verifyToken");
 const getUserController = require("../../Controllers/Auth/GetUserController");
+const refreshTokenController = require("../../Controllers/Auth/RefreshTokenController");
 
 router.post("/register", registerController);
 router.post("/login", loginController);
 router.get("/user", verifyToken, getUserController);
+router.post("/refresh", verifyToken, refreshTokenController);
 
 module.exports = router;
