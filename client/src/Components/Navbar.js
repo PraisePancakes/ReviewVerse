ae1fe95e606ed6ec4a8586ee2f3ced84878ec2bc
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2023-09-28 20:14:34 -0400

    Navbar base implemented -styling

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -5,6 +5,29 @@ import { useScroll } from "../Hooks/useScroll";
 const Navbar = () => {
   const scrollPosition = useScroll();
 
+  const navLinks = [
+    {
+      title: "Find",
+      id: 1,
+      path: "/reviews/find",
+    },
+    {
+      title: "Saved",
+      id: 2,
+      path: "/reviews/saved",
+    },
+    {
+      title: "Trending",
+      id: 3,
+      path: "/reviews/trending",
+    },
+    {
+      title: "About Us",
+      id: 4,
+      path: "/about",
+    },
+  ];
+
   return (
     <div>
       <div
@@ -15,12 +38,24 @@ const Navbar = () => {
         }`}
       >
         {" "}
-        <nav className="text-white mx-5 flex">
+        <nav className="text-white mx-5 flex gap-5 items-center">
           <Link to="/" className="cursor-pointer">
             <h1 className={`${scrollPosition > 0 ? "text-black" : ""}`}>
               REVIEW VERSE
             </h1>
           </Link>
+          <ul className="lg:flex gap-5 hidden">
+            {navLinks.map((navItem) => {
+              return (
+                <Link key={navItem.id} path={`${navItem.path}`}>
+                  <li className="font-bold text-lg">{navItem.title}</li>
+                </Link>
+              );
+            })}
+          </ul>
+          <div className="flex flex-1 font-bold text-lg justify-end">
+            Profile
+          </div>
         </nav>
       </div>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./src/App";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./src/Context/AuthContext";
+import { AuthProvider } from "./Context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
