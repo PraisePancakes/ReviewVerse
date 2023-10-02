1cb1d80873f835b7bd2f6e750d10ca254c7cbce9
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2023-10-02 02:13:38 -0400

    Implemented carousel for movies

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,44 +1,75 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  //to-do refactor this code up, possible back-end request, examine cors options
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MzEzNmQ3ZDQwZGExYjA5NmFjOTA1YWQwN2I4YjY5ZCIsInN1YiI6IjY1MTkwMWRhMDcyMTY2MDBmZjM1ZGQ4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r9bxDYaMp-tUVp_BHyfw-RlQzRJRvlgDS-ILs-u0_bg",
-    },
+  const [movieSlides, setMovieSlides] = useState([]);
+  const [error, setError] = useState(null);
+  const [slide, setSlide] = useState(0);
+
+  const REACT_APP_MOVIES_API_CONNECTION =
+    process.env.REACT_APP_MOVIES_API_CONNECTION;
+  const REACT_APP_MOVIES_API_ACCESS_TOKEN =
+    process.env.REACT_APP_MOVIES_API_ACCESS_TOKEN;
+
+  const chunkifyMovies = (movies) => {
+    let chunkSize = 3;
+    let temp = [];
+    let result = [];
+
+    for (let i = 0; i < movies?.length; i++) {
+      temp.push(movies[i]);
+      if (i % 3 === chunkSize - 1) {
+        result.push(temp);
+        temp = [];
+      }
+    }
+    setMovieSlides(result);
   };
 
-  const getMovies = async () => {
-    try {
-      fetch(
-        "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => setMovies(response.results))
-        .catch((err) => console.error(err));
-    } catch (error) {
-      console.log(error);
-    }
+  const nextSlide = (slide) => {
+    if (slide === movieSlides.length - 1) {
+      setSlide(0);
+    } else setSlide(slide + 1);
   };
+
+  const prevSlide = (slide) => {
+    if (slide === 0) {
+      setSlide(movieSlides.length - 1);
+    } else setSlide(slide - 1);
+  };
+
+  const getMovies = useCallback(async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `${REACT_APP_MOVIES_API_ACCESS_TOKEN}`,
+      },
+    };
+
+    try {
+      fetch(`${REACT_APP_MOVIES_API_CONNECTION}`, options)
+        .then((response) => response.json())
+        .then((response) => chunkifyMovies(response.results))
+        .catch((error) => setError(error.response.data.message));
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  }, [REACT_APP_MOVIES_API_CONNECTION, REACT_APP_MOVIES_API_ACCESS_TOKEN]);
 
   useEffect(() => {
     getMovies();
-    return () => getMovies();
-  }, []);
+  }, [getMovies]);
 
-  console.log(movies);
+  console.log(movieSlides);
   return (
-    <div>
-      <ul>
-        {movies.map((movie) => {
+    <div className="mx-10">
+      <h1 className="text-black">MOVIES</h1>
+      <ul className="flex gap-10 justify-center">
+        <button onClick={() => prevSlide(slide)}>{"<"}</button>
+        {movieSlides[slide]?.map((movie) => {
           return <li key={movie.id}>{movie.title}</li>;
         })}
+        <button onClick={() => nextSlide(slide)}>{">"}</button>
       </ul>
     </div>
   );
